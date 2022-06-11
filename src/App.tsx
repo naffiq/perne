ec8b14140ef2cd2a06f22e8f3ac69451033ceb45
naffiq <naffiq@gmail.com>
2022-06-11 17:05:46 +0600

    feat: updated react version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Game from "./components/Game";
+import Typer from "./components/Typer";
+import { booksTexts } from "./data/texts";
+import { GameStatus } from "./interface/GameStatus";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-gray-900">
+      <div className="container mx-auto ">
+        <header className="py-4">
+          <h1 className="text-lg mb-8 text-white">
+            perne - simple typing game
+          </h1>
+        </header>
+
+        <Game />
+      </div>
     </div>
   );
 }
